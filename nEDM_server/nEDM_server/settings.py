484711fd21d11efbd15b04849accb8ad12a8f48f
dougUCN <douglas.kt.wong@gmail.com>
2022-03-28 17:35:42 -0500

    Removes autogenerated django encryption key and moves it to a non-public, untracked file

--- nEDM_server/nEDM_server/settings.py
+++ nEDM_server/nEDM_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from security import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=c6q-4$lt&!yro8r_o331%=+o-#c!u&rbl4fkf892+v6eq@zq$'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
