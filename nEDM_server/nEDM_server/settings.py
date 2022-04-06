8580f67f6e66d74cb0e4cdd6b6822ce3fb456b6d
dougUCN <douglas.kt.wong@gmail.com>
2022-04-06 17:38:08 -0600

    Enables Cors on BE to allow for a locally hosted FE on a different port

--- nEDM_server/nEDM_server/settings.py
+++ nEDM_server/nEDM_server/settings.py
@@ -35,9 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ariadne_django',
     'histograms',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +85,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# For FE and BE hosted locally on different ports
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
