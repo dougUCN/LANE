025b7790a07ef3a9d0afe07e9b760d8d0727309d
dougUCN <douglas.kt.wong@gmail.com>
2022-04-10 14:27:51 -0600

    Adds second database for users

--- nEDM_server/nEDM_server/settings.py
+++ nEDM_server/nEDM_server/settings.py
@@ -83,6 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'users': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'users/users.sqlite3',
     }
 }
 
